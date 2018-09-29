4361ae5476f2508d1a0320192671fe2ca8c53a38
mame-n <nakauchi.s@gmail.com>
2018-09-30 01:43:44 +0900

    first commit

---
+++ mainMictobit.js
@@ -0,0 +1,7 @@
+basic.forever(() => {
+    if (input.buttonIsPressed(Button.A)) {
+        pins.digitalWritePin(DigitalPin.P1, 1)
+    } else {
+        pins.digitalWritePin(DigitalPin.P1, 0)
+    }
+})
